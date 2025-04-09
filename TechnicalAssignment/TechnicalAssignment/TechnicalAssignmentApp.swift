82b3268b674b5b181e01cbbc85173056722e30e4
jamienguyendeveloper <thuannguyendeveloper@gmail.com>
2025-04-09 10:15:30 +0700

    Init project

---
+++ TechnicalAssignment/TechnicalAssignment/TechnicalAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalAssignmentApp.swift
+//  TechnicalAssignment
+//
+//  Created by Jamie on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
